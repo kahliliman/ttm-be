97db8905ae6febc65c8d23daf33e0205119a4531
Kahlil Iman <kahlil.iman@gmail.com>
2021-03-30 11:41:33 +0700

    setup firebase

---
+++ src/configs/firebase/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE,
+  messagingSenderId: process.env.MSG_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import express from "express";
 import logger from "morgan";
+import firebase from "firebase";
+import db from "./models";
+import firebaseConfig from "./configs/firebase";
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3100;
+
+db.sequelize
+  .authenticate()
+  .then(() =>
+    console.log(
+      `Database ${process.env.DB_DATABASE} connected on port:${process.env.DB_PORT}`
+    )
+  )
+  .catch((err) => console.log(`Database connection error: ${err}`));
+
+firebase.initializeApp(firebaseConfig);
 
 app.use(logger("dev"));
 app.use(express.json());
